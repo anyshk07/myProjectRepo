148ae15b146f72c740cb58c2f50148cf37f3a813
anyshk07 <70767398+anyshk07@users.noreply.github.com>
2021-03-22 18:31:54 +0530

    Adding subtract Method

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -11,5 +11,9 @@ public class Calculator {
 	public static void add(int num1, int num2){
 		System.out.println(num1+num2);
 	}
+	
+	public static void difference(int num1, int num2){
+		System.out.println(num2-num1);
+	}
 
 }
